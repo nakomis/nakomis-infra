8dff14ac4573238bef7b99aff28e317e004b5dad
Martin Harris <1488244+nakomis@users.noreply.github.com>
2026-09-16 15:23:18 +0100

    feat(tracker): allow-list lapcat CI roles (LAPC-11)

--- infra/lib/deployment-tracker-stack.ts
+++ infra/lib/deployment-tracker-stack.ts
@@ -31,6 +31,8 @@ const ALLOWED_CI_ROLE_ARNS = [
   'arn:aws:iam::637423226886:role/nakomis-blog-pipeline-github-ci-prod',
   'arn:aws:iam::975050268859:role/nakomis-home-infra-github-ci-sandbox',
   'arn:aws:iam::637423226886:role/nakomis-home-infra-github-ci-prod',
+  'arn:aws:iam::975050268859:role/nakomis-lapcat-github-ci-sandbox',
+  'arn:aws:iam::637423226886:role/nakomis-lapcat-github-ci-prod',
 ];
 
 export class DeploymentTrackerStack extends cdk.Stack {
